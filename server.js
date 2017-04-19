8afcf3d08caddb96297cf39e677c42a6ad499e9f
Nick Rutten <hello@nickrttn.me>
2017-04-19 14:45:04 +0200

    oAuth

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const https = require('https');
+const request = require('request');
 const debug = require('debug')('express');
 const express = require('express');
 
@@ -11,13 +13,31 @@ app.set('view engine', 'ejs');
 app.set('port', process.env.PORT);
 
 // Static file serving
-app.use('/assets', express.static(path.join(__dirname + '/build')))
+app.use('/assets', express.static(path.join(__dirname, '/build')));
 
 app.get('/', (req, res) => {
-	res.render('pages/index');
+	const oauthURL = `${process.env.IG_OAUTH}/authorize/?client_id=${process.env.IG_CLIENT}&redirect_uri=http://localhost:3000/oauth&response_type=code`;
+	res.render('pages/index', {oauthURL});
+});
+
+app.get('/oauth', (req, res) => {
+	request.post({
+		url: `${process.env.IG_OAUTH}/access_token`,
+		json: true,
+		form: {
+			client_id: process.env.IG_CLIENT, // eslint-disable-line camelcase
+			client_secret: process.env.IG_SECRET, // eslint-disable-line camelcase
+			grant_type: 'authorization_code', // eslint-disable-line camelcase
+			redirect_uri: 'http://localhost:3000/oauth', // eslint-disable-line camelcase
+			code: req.query.code
+		}
+	}, (err, res, body) => {
+		if (err) debug(err);
+		debug(res, body);
+	});
 });
 
 app.listen(app.get('port'), (err) => {
-	if (err) debug(err);
+	if (err) debug(err); // eslint-disable-line curly
 	debug(`listening on http://localhost:${app.get('port')}`);
 });
