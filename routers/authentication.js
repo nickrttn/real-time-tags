3b03108c86864008b9273806d5fd21323808ccb3
Nick Rutten <hello@nickrttn.me>
2017-04-28 10:23:14 +0200

    remove isomorphic-fetch and use a single request library

--- routers/authentication.js
+++ routers/authentication.js
@@ -1,27 +1,30 @@
 const debug = require('debug')('express');
 const express = require('express');
-const request = require('request');
+
+const request = require('../lib/request');
 const user = require('../db/user');
 
 const router = new express.Router();
 
 router.get('/', (req, res) => { // eslint-disable-line no-unused-vars
-	request.post({
-		url: `${process.env.IG_OAUTH}/access_token`,
-		json: true,
-		form: {
-			client_id: process.env.IG_CLIENT, // eslint-disable-line camelcase
-			client_secret: process.env.IG_SECRET, // eslint-disable-line camelcase
-			grant_type: 'authorization_code', // eslint-disable-line camelcase
-			redirect_uri: 'http://localhost:3000/oauth', // eslint-disable-line camelcase
-			code: req.query.code
-		}
-	}, (err, postRes, body) => {
+	const oauthObj = {
+		client_id: process.env.IG_CLIENT, // eslint-disable-line camelcase
+		client_secret: process.env.IG_SECRET, // eslint-disable-line camelcase
+		grant_type: 'authorization_code', // eslint-disable-line camelcase
+		redirect_uri: 'http://localhost:3000/oauth', // eslint-disable-line camelcase
+		code: req.query.code
+	};
+
+	function onauth(err, oauthRes) {
 		if (err) debug(err); // eslint-disable-line curly
-		user.save(body);
-		req.session.userId = body.user.id;
-		res.redirect('/');
-	});
+		if (oauthRes.status === 200) {
+			user.save(oauthRes.body);
+			req.session.userId = res.body.user.id;
+			res.redirect('/');
+		}
+	}
+
+	request.postForm(`${process.env.IG_OAUTH}/access_token`, oauthObj, onauth);
 });
 
 module.exports = router;
